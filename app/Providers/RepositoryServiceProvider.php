1eff7a9e73ff00f65d78a505a53c023a199a8314
Muhammad Arslan <mughalarslan996@gmail.com>
2025-10-02 09:49:45 +0500

    update repository service providers

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -13,9 +13,6 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         // $repositories = [
         //     \App\Repositories\Contracts\CustomerRepositoryInterface::class => \App\Repositories\Eloquent\CustomerRepository::class,
-        //     \App\Repositories\Contracts\DriverRepositoryInterface::class   => \App\Repositories\Eloquent\DriverRepository::class,
-        //     \App\Repositories\Contracts\CompanyRepositoryInterface::class  => \App\Repositories\Eloquent\CompanyRepository::class,
-        //     \App\Repositories\Contracts\OrderRepositoryInterface::class    => \App\Repositories\Eloquent\OrderRepository::class,
         // ];
 
         // foreach ($repositories as $interface => $implementation) {
